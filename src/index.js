8387fb36c232ea0a0f0eb971b1f402833e3d1e8d
Marina Oliveira <marina09oliveira@gmail.com>
2020-11-25 19:32:27 -0300

    Pure Redux: Counter

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { createStore } from 'redux';
+
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+
+number.innerText = 0;
+
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
+// reducer
+const countModifier = (count = 0, action) => {
+  // actions = tem que ser um obj e ter um type, nos comunicamos  com o reducer atravez de actions
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+// listen for changes
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
