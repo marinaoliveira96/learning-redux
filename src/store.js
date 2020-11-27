b3350dcb640accd871836e9b1d6f9b9d4a73f714
Marina Oliveira <marina09oliveira@gmail.com>
2020-11-27 17:43:20 -0300

    Redux Toolkit

--- src/store.js
+++ src/store.js
@@ -1,32 +1,30 @@
 import { createStore } from 'redux';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
-const ADD = 'ADD';
-const DELETE = 'DELETE';
+const addToDo = createAction('ADD');
+const deleteToDo = createAction('DELETE');
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
-
-const reducer = (state = [], action) => {
+/* const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      console.log(action.payload);
+      // payload = o que está sendo enviado
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
-};
+}; */
+
+const reducer = createReducer([], {
+  //permite modificar o state, mas só se voce fizer direto, se for retornar algo tem que ser um novo state
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const store = createStore(reducer);
 
